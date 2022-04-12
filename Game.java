59434e8d3c4749084ab25753c098ec70ba5a0581
Salai777 <100633345+Salai777@users.noreply.github.com>
2022-04-12 14:26:05 +0200

    Update Game.java

--- Game.java
+++ Game.java
@@ -12,7 +12,7 @@ public class Game {
     public static final int Y_SCALE = 10; //dividing canvas up vertically (helps with placement of drawings)
 
     public static final double ENEMY_RADIUS = 0.5; //radius of enemy
-    public static final double PLAYER_RADIUS = 0.25; //radius of player
+    public static final double PLAYER_RADIUS = 0.35; //radius of player
     public static final double MISSILE_RADIUS = 0.08; //radius of missile
 
     public static double VX_ENEMY = 0.060; // speed of enemies
@@ -22,16 +22,26 @@ public class Game {
     public int ENEMY_KILLED = 0; // count number of enemies killed to advance to next level
 
     public ArrayList<Missile> MISSILES = new ArrayList<Missile>(); // array list of missiles that can be created and removed where necessary
-    public static final double MISSILE_SPEED = 0.15; // speed at which missiles fire
+    public ArrayList<Missile> ENEMY_MISSILES = new ArrayList<Missile>(); // array list of enemy missiles
 
-    public static long FRAME_COUNT = 0; // allows while loop, which keeps the game running, to execute a number of times before missile appears on screen
-    // allows missiles to appear spaced apart
+    public static final double MISSILE_SPEED = 0.15; // speed at which missiles fire
+    public static final double ENEMYMISSILE_SPEED = 0.08;
+
+
+    public static long FRAME_COUNT = 0;// allows while loop, which keeps the game running, to execute a number of times before missile appears on screen
+                                       // allows missiles to appear spaced apart
+    public static long FRAME_COUNT1 = 0;  // allows enemy missiles to be spaced out
+    public static long FRAME_COUNT2 = 0;                   //frame count for animations
+
+
 
     public int LIVES = 3; // player lives
     public int SCORE = 0; // score while game is running
 
-    public double LEVELUP = 1.5; // enemy speed multiplier when player moves up a level
+    public double LEVELUP = 1.2; // enemy speed multiplier when player moves up a level
     public int SCORE_INCREASE = 1; // score multiplier when player moves up a level
+    public double MISSILE_PERIOD = 2.0; // period for enemy missiles to occur
+
 
     public void newGame() {
         Menu menu = new Menu();
@@ -53,11 +63,13 @@ public class Game {
             StdDraw.text(1, 9.8, "Level: " + SCORE_INCREASE);
 
             FRAME_COUNT++;
+            FRAME_COUNT1++;
 
             if (StdDraw.isKeyPressed(82)) { // 82 = ascii for "r"
                 LIVES = 3;
                 reset();
                 removeMissiles();
+                removeENEMYMissiles();
                 newGame();
             }
 
@@ -65,15 +77,19 @@ public class Game {
                 menu.quit();
             }
 
-            if (enemyByPlayer(enemies, player)) {
+            if (enemyByPlayer(enemies, player)) {  //player killed by enemies
                 ENEMY_KILLED = 0;
                 LIVES--;
-                if (LIVES <= 0) {
+                if (LIVES <= 0) {   //ADD REMOVE MISSILE FUNCTIONS?
+                    gameover();
                     writeScore(SCORE);
                     reset();
                     break;
                     //game over
                 }
+                removeMissiles();
+                removeENEMYMissiles();
+                livesdecrease(LIVES);
                 newGame();
             }
 
@@ -84,11 +100,14 @@ public class Game {
             updateMissiles();
             destroy(enemies);
 
-            if (ENEMY_KILLED == (ENEMY_NUM * ENEMY_NUM)) {
+            if (ENEMY_KILLED == (ENEMY_NUM * ENEMY_NUM)) { //pass to next level when all enemies are destroyed
                 ENEMY_KILLED = 0;
                 VX_ENEMY *= LEVELUP;
                 SCORE_INCREASE++;
+                MISSILE_PERIOD *= 0.8; //increase rate of enemy missile fire
                 removeMissiles();
+                removeENEMYMissiles();
+                nextlevel(SCORE_INCREASE);
                 newGame();
             }
 
@@ -101,25 +120,41 @@ public class Game {
 
     }
 
-    public void removeMissiles() {
+    public void removeMissiles() { //remove all missiles
         for (int i = 0; i < MISSILES.size(); i++) {
             MISSILES.remove(i);
         }
     }
 
-    public boolean enemyByPlayer(Enemy[][] enemies, PlayerCharacter player) {
-        boolean answer = false;
+    public void removeENEMYMissiles() { //remove all enemy missiles
+        for (int i = 0; i < ENEMY_MISSILES.size(); i++) {
+            ENEMY_MISSILES.remove(i);
+        }
+    }
 
-        for (int i = 0; i < ENEMY_NUM; i++) {
+    public boolean enemyByPlayer(Enemy[][] enemies, PlayerCharacter player) {
+        boolean answer2 = false;
+        boolean answer1 = false;
+
+        for (int i = 0; i < ENEMY_NUM; i++) { //killed by enemies
             for (int j = 0; j < ENEMY_NUM; j++) {
                 if ((enemies[i][j].getActive()) && (enemies[i][j].getY() - ENEMY_RADIUS < player.getY() + 1)) {
-                    answer = true;
+                    answer1 = true;
                     i = ENEMY_NUM;
                     j = ENEMY_NUM;
                 }
             }
         }
-        return answer;
+
+        for (int i = 0; i < ENEMY_MISSILES.size(); i++) { //killed by enemy missiles
+            if ( Math.sqrt( Math.pow(ENEMY_MISSILES.get(i).getY() - player.getY(),2) + Math.pow(ENEMY_MISSILES.get(i).getX() - player.getX(),2)) < MISSILE_RADIUS + PLAYER_RADIUS){
+                answer2 = true;
+            }
+        }
+
+        if (answer1||answer2)
+            return true;
+        else return false;
     }
 
     public void createPlayer(PlayerCharacter player) { //draws player on canvas
@@ -179,23 +214,56 @@ public class Game {
 
 
     public void updateMissiles() {  //update and draw missile on screen
-        for (int i = 0; i < MISSILES.size(); i++) {
 
-            if (MISSILES.get(i).getAngle() == 0) {
-                MISSILES.get(i).setY(MISSILES.get(i).getY() + MISSILE_SPEED);
-            } else {
-                double theta = (double) MISSILES.get(i).getAngle() * Math.PI / 180;
-                MISSILES.get(i).setX(MISSILES.get(i).getX() + (Math.sin(theta) * MISSILE_SPEED));
-                MISSILES.get(i).setY(MISSILES.get(i).getY() + (Math.cos(theta) * MISSILE_SPEED));
-            }
+        for (int i = 0; i < MISSILES.size(); i++) { //update missile position and display;
+
+            double theta = (double) MISSILES.get(i).getAngle() * Math.PI / 180;
+            MISSILES.get(i).setX(MISSILES.get(i).getX() + (Math.sin(theta) * MISSILE_SPEED));
+            MISSILES.get(i).setY(MISSILES.get(i).getY() + (Math.cos(theta) * MISSILE_SPEED));
 
             StdDraw.setPenColor(Color.WHITE);
-            StdDraw.filledCircle(MISSILES.get(i).getX(), MISSILES.get(i).getY() + PLAYER_RADIUS, MISSILE_RADIUS);
+            StdDraw.filledCircle(MISSILES.get(i).getX(), MISSILES.get(i).getY(), MISSILE_RADIUS);
             StdDraw.setPenColor(Color.RED);
+            System.out.println(MISSILES.get(i).getX() + " " + MISSILES.get(i).getY());
 
-            if (MISSILES.get(i).getY() + MISSILE_RADIUS > Y_SCALE)
+        }
+
+        for (int i = 0; i < ENEMY_MISSILES.size(); i++) { //update enemy missile position and display;
+
+            ENEMY_MISSILES.get(i).setY(ENEMY_MISSILES.get(i).getY() - ENEMYMISSILE_SPEED);
+            StdDraw.setPenColor(Color.YELLOW);
+            StdDraw.filledCircle(ENEMY_MISSILES.get(i).getX(), ENEMY_MISSILES.get(i).getY(), MISSILE_RADIUS);
+            StdDraw.setPenColor(Color.RED);
+            System.out.println(ENEMY_MISSILES.get(i).getX() + " " + ENEMY_MISSILES.get(i).getY());
+
+        }
+
+        for (int i = 0; i < MISSILES.size(); i++) { //remove out of bounds missiles
+            if (MISSILES.get(i).getY() + MISSILE_RADIUS > Y_SCALE|| MISSILES.get(i).getX() + MISSILE_RADIUS > X_SCALE || MISSILES.get(i).getX() - MISSILE_RADIUS < 0)
                 MISSILES.remove(i);
         }
+
+        for (int i = 0; i < ENEMY_MISSILES.size(); i++) { // remove out of bounds enemy missiles
+            if ((ENEMY_MISSILES.get(i).getY() - MISSILE_RADIUS) < 0)
+                ENEMY_MISSILES.remove(i);
+        }
+
+        /*int a = MISSILES.size();
+        int b = ENEMY_MISSILES.size();
+
+
+        for (int i = 0; i < a; i++) {    //missile and enemy missile collision
+            for (int j = 0; j < b; j++) {
+                if ( Math.sqrt( Math.pow(ENEMY_MISSILES.get(j).getY() - MISSILES.get(i).getY(),2) + Math.pow(ENEMY_MISSILES.get(j).getX() - MISSILES.get(i).getX(),2)) < MISSILE_RADIUS*2){
+                    ENEMY_MISSILES.remove(j);
+                    MISSILES.remove(i);
+                    SCORE += 5;
+                    i = a;
+                    j = b;
+                }
+            }
+        } */
+
     }
 
 
@@ -257,6 +325,19 @@ public class Game {
 
             }
         }
+
+        if ( !(MISSILE_PERIOD == 2.0) && FRAME_COUNT1 > MISSILE_PERIOD*40) { //creates random enemy missile
+            for (int i = 0; i < N*N; i++) {
+                int n = (int) (Math.random()*N);
+                int m = (int) (Math.random()*N);
+                if (enemies[n][m].getActive()) {
+                    ENEMY_MISSILES.add(new Missile(enemies[n][m].getX(), enemies[n][m].getY(), 0));
+                    FRAME_COUNT1 = 0;
+                    break;
+                }
+            }
+        }
+
     }
 
     public static void writeScore(int score) { //writes score achieved during the game to text file that stores all scores ever achieved
@@ -288,5 +369,74 @@ public class Game {
         VX_ENEMY = 0.060;
         VX_PLAYER = 0.060;
         SCORE_INCREASE = 1;
+        MISSILE_PERIOD = 2.0;
     }
+
+    public static void gameover() { //animation for game over
+        Font font1 = new Font("Serif", Font.BOLD, 50);
+        StdDraw.setFont(font1);
+        StdDraw.setPenColor(StdDraw.BOOK_RED);
+
+        while (FRAME_COUNT2 < 110) {
+
+            StdDraw.clear(Color.BLACK);
+            StdDraw.enableDoubleBuffering();
+
+            if ((FRAME_COUNT2 < 20) || ((FRAME_COUNT2 > 30) && (FRAME_COUNT2 < 50)) || ((FRAME_COUNT2 > 60) && (FRAME_COUNT2 < 80)) || ((FRAME_COUNT2 > 90) && (FRAME_COUNT2 < 110)) )
+                StdDraw.text(5, 6.5, "GAME OVER");
+
+            FRAME_COUNT2++;
+            StdDraw.show();
+            StdDraw.pause(20);
+
+        }
+        FRAME_COUNT2 = 0;
+    }
+
+    public static void nextlevel(int level) { //animation for next level
+        Font font1 = new Font("Serif", Font.BOLD, 50);
+        StdDraw.setFont(font1);
+        StdDraw.setPenColor(StdDraw.GREEN);
+
+        while (FRAME_COUNT2 < 40) {
+
+            StdDraw.clear(Color.BLACK);
+            StdDraw.enableDoubleBuffering();
+
+            if (FRAME_COUNT2 < 20)
+                StdDraw.text(5, 6.5, "LEVEL: " + (level - 1));
+            else
+                StdDraw.text(5, 6.5, "LEVEL: " + (level));
+
+            FRAME_COUNT2++;
+            StdDraw.show();
+            StdDraw.pause(20);
+
+        }
+        FRAME_COUNT2 = 0;
+    }
+
+    public static void livesdecrease(int lives) { //animation for lives decrease
+        Font font1 = new Font("Serif", Font.BOLD, 50);
+        StdDraw.setFont(font1);
+
+        while (FRAME_COUNT2 < 60) {
+
+            StdDraw.clear(Color.BLACK);
+            StdDraw.enableDoubleBuffering();
+
+            if (FRAME_COUNT2 < 30)
+                StdDraw.text(5, 6.5, "LIVES: " + (lives + 1));
+            else
+                StdDraw.text(5, 6.5, "LIVES: " + lives);
+
+            FRAME_COUNT2++;
+            StdDraw.show();
+            StdDraw.pause(20);
+
+        }
+        FRAME_COUNT2 = 0;
+    }
+
+
 }
